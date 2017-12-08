82e3524c2e84acc7b2be991b4c8aa0ed46c9b034
Guest User <Guest@Epicodus-8E.local>
2017-12-08 15:32:46 -0800

    update function using a return value

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 //business logic
-var numberToOutput = function(numberInput) {
-  var numberInput = parseInt($("#userInput").val());
+var numberToOutput = function(personInputConverter) {
+  personInput = parseInt(personInput);
   var numbersArray = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
+  var zeros = ["0", "10", "20"];
   var ones = ["1","11","21"];
   var divisableByThree = ["3", "6", "9", "12", "15", "18", "21", "23", "26", "29"];
   var result = "";
 
 
-if (!(numberInput.match(/[0-9]/))) {
+  if (!(personInputConverter.match(/[0-9]/))) {
     return "Error!"; //not a number
   } else if (personInput > 30) {
-      return "Error, I cant count that high!";
+      return "Error, I cant count that high!"; //number over 30
   }
   else {
     for (var i=0; i<numbersArray.length; i++) { //first loop
@@ -24,19 +25,20 @@ if (!(numberInput.match(/[0-9]/))) {
       }
     }
     for (var i=0; i<numbersArray.length; i++) { //third loop
-      if (personInput ===  divisableByThree[i]) {
+      if (personInput === divisableByThree[i]) {
         return "I'm sorry Dave. I'm afraid I can't do that!";
       }
     }
-  return result;
+    return result;
+  }
 }
 
 function checkInput(input) {
   if (input.match(/[0-9]/)) {
-   return true;
+    return true;
 }  else {
-     return false;
-   }
+    return false;
+  }
 }
 
 ///user interface logic
@@ -46,16 +48,16 @@ $(document).ready(function() {
     event.preventDefault();
 
 
-    var personsInput = $("userInputNumber").val();
-    var checker = checkInput(inputNumber)
+    var personInput = $("#userInputNumber").val();
+    var checker = checkInput(personInput)
 
     if (checker === true) {
-
-      var result = numberToOutput(personsInput); //box containing input
+debugger;
+      var result = numberToOutput(personInput); //box containing input
       $("#result").text(result)  //send result to user
     }
     else {
-      $("#result").text("I can't count that high!") debugger;
+      $("#result").text("I can't count that high!")
     }
   });
 });
