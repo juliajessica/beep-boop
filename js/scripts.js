c77fd7e4702241fe607068a551b4edf880ffc028
Guest User <Guest@Epicodus-8E.local>
2017-12-08 14:38:11 -0800

    update for, if, and retur in loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,42 @@
 //business logic
 var numberToOutput = function(numberInput) {
   var numberInput = parseInt($("#userInput").val());
-  var numbersArray = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
-  var zeros = ["0", "10", "20", "30"];
+  var numbersArray = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
   var ones = ["1","11","21"];
   var divisableByThree = ["3", "6", "9", "12", "15", "18", "21", "23", "26", "29"];
   var result = "";
 
-  if (!(numberInput.match(/[0-9]/))) {
+
+if (!(numberInput.match(/[0-9]/))) {
     return "Error!"; //not a number
-  } else if (number > 30) {
+  } else if (personInput > 30) {
       return "Error, I cant count that high!";
   }
   else {
     for (var i=0; i<numbersArray.length; i++) { //first loop
-      if (numberInput === zeros[i]) {
+      if (personInput === zeros[i]) {
         return "Beep!";
       }
     }
     for (var i=0; i<numbersArray.length; i++) { //second loop
-      if (numberInput === ones[i]) {
+      if (personInput === ones[i]) {
         return "Boop!";
       }
     }
     for (var i=0; i<numbersArray.length; i++) { //third loop
-      if (numberInput ===  divisableByThree[i]) {
+      if (personInput ===  divisableByThree[i]) {
         return "I'm sorry Dave. I'm afraid I can't do that!";
       }
     }
   return result;
+}
+
+function checkInput(input) {
+  if (input.match(/[0-9]/)) {
+   return true;
+}  else {
+     return false;
+   }
 }
 
 ///user interface logic
@@ -43,7 +51,7 @@ $(document).ready(function() {
 
     if (checker === true) {
 
-      var result = numberInput(personsInput); //box containing input
+      var result = numberToOutput(personsInput); //box containing input
       $("#result").text(result)  //send result to user
     }
     else {
@@ -53,19 +61,7 @@ $(document).ready(function() {
 });
 
 
-//     for (var i=0; i <number; index++) {
-//   if (numberArray[i] !== 0) {
-//     $("#outputResults").append(numberArray[i] + " ").css("font-size", index + "px;");
-//   }
-// }
 
-
-
-
-// var numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-
-// //
-//
 
       // var result= "";
 // var str = "";
