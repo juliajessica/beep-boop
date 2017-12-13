77405b3b91cd2cd8ef20a0e3619972c03e959b16
Guest User <Guest@Epicodus-8E.local>
2017-12-13 07:19:50 -0800

    change jquery format

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,44 @@
 //business logic
 var numberToOutput = function(userInputNumber) {
   personInput = parseInt(userInputNumber);
-  var numbersArray = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"];
-  var zeros = ["0", "10", "20"];
-  var ones = ["1","11","21"];
-  var divisableByThree = ["3", "6", "9", "12", "15", "18", "21", "23", "26", "29"];
+  var numbersArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  var zeros = [0, 10, 20];
+  var ones = [1,11,21];
+  var divisableByThree = [3, 6, 9, 12, 15, 18, 21, 23, 26, 29];
   var result = [];
+  debugger;
 
 
-  if (!(userInputNumber.match(/[0-9]/))) { //number over 30
-    return "Error!"; //not a number
-  } else if (userInputNumber > 30) {
+  // if ((userInputNumber.contains(^/[0-9]/))) { //number over 30
+  //
+
+  //   return "Error!"; //not a number
+   if (userInputNumber > 30) {
     $(".result").text("Error, I cant count that high!");
       // return "Error, I cant count that high!";
-  }
-  else {
-    for (var i = 0; i < numbersArray.length; i++) { //first loop
+  } else if {
+    for (var i = 0; i < userInputNumber.length; i++) { //first loop
       if (userInputNumber === zeros[i]) {
         $(".result").text("Beep!").show();
         //return "Beep!";
       }
-    }
-    for (var i = 0; i < numbersArray.length; i++) { //second loop
+    // } for (var i = 0; i < numbersArray.length; i++) { //second loop
       if (userInputNumber === ones[i]) {
         $(".result").text("Boop!").show();
         // return "Boop!";
       }
-    }
-    for (var i = 0; i < numbersArray.length; i++) { //third loop
+    // } for (var i = 0; i < numbersArray.length; i++) { //third loop
       if (userInputNumber === divisableByThree[i]) {
         $(".result").text("I'm sorry Dave. I'm afraid I can't do that!").show();
+        console.log(result);
         // return "I'm sorry Dave. I'm afraid I can't do that!";
       }
-    }
-    // return result;
-    $(".result")
+    }// return result;
   }
 }
 
 function checkInput(input) {
-  if (input.match(/[0-9]/)) {
+  if (input.indexof(/[0-9]/)) {
     return true;
 }  else {
     return false;
@@ -54,7 +53,7 @@ $(document).ready(function() {
     var userInputNumber = parseInt($("input#userInputNumber").val());
     var output = numberToOutput(userInputNumber);
 
-$(".result").text(output);
+    $("<li>" + output + "</li>").appendTo(".result");
 
 //     var personInput = $("#userInputNumber").val();
 //     var checker = checkInput(personInput)
