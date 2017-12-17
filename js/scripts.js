bb63e45230a366233029a098cdd9e6b6992f44cb
julia sheremet <juliajessicasher@gmail.com>
2017-12-17 13:04:49 -0800

    debug loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 //business logic
+var results = [];
+
 var numberToOutput = function(userInputNumber) {
-  var result = [];
 
   for (var i = 2; i < userInputNumber.length -1; i++) { //first loop
-    if (userInputNumber.toString().includes("0") ) {
+    if (userInputNumber.toString().includes("0")) {
       results.push("Beep!");
-    } else if (userInputNumber.toString().includes("1") { //second loop
+    } else if (userInputNumber.toString().includes("1")) { //second loop
       results.push("Boop!");
-        // return "Boop!";
-    } else if (userInputNumber.string().includes() { //modulo needs to operate on a number
+    } else if (userInputNumber.toString().includes(userInputNumber % 3 === 0 && userInputNumber/3 >= 1)) { //modulo needs to operate on a number
         results.push("I'm sorry Dave. I'm afraid I can't do that!");
+        // cosole.log("I'm sorry Dave. I'm afraid I can't do that!");
+
     }
     else {
       results.push(i); //no other case is true, push number!
@@ -18,14 +20,6 @@ var numberToOutput = function(userInputNumber) {
   return results;
 }
 
-// function checkInput(input) {
-//   if (input.indexof(/[0-9]/)) {
-//     return true;
-// }  else {
-//     return false;
-//   }
-// }
-
 ///user interface logic
 
 $(document).ready(function() {
@@ -33,29 +27,32 @@ $(document).ready(function() {
     event.preventDefault();
     var finalNumber = parseInt($("input#userInputNumber").val());
     var output = numberToOutput(finalNumber); //output is an array!!!!!!!!!!
+    $("ul#result").show();
+    // debugger;
+    // var listArray = function() {
+        // (var i = 0; i < userInputNumber.length; i++) {
+    for (var i = 0; i < results.length; i++) {
+      $("<li>" + results[i] + "</li>").appendTo("#result");
 
-    // if (finalNumber = 0 || finalNumber = 10 || finalNumber = 20) {
-    //   $(".result").text(output);
-    // } else (finalNumber = 1 || finalNumber = 11 || finalNumber = 21) {
-    //   $(".result").text(output);
-    // } else (finalNumber = 3 || finalNumber = 6 || finalNumber = 9 || finalNumber = 12 || finalNumber = 15 || finalNumber = 18 || finalNumber = 21 || finalNumber = 23 || finalNumber = 26 || finalNumber = 29) {
-    //   $(".result").text(output);
-    // }
+      // $("<li>" + output + "</li>").appendTo("ul#result");
+
+    }
+  });
+});
+    // output = results;
+    // console.log(finalNumber);
 
 
+//     // results.forEach(function(result) {
+//     // $("<li>" + result + "</li>").appendTo(".result");
+//     // });
+// });
+// });
+    // $("<li>" + output[i] + "</li>").appendTo(".result");
   //["beep", "boop", "dave", 4, 5] etc....
-
-  //for loop to show this nicely.
-    $("<li>" + output[i] + "</li>").appendTo(".result");
-    // $(".result").text("Beep!").push();
 
 
     // output[0] this could be replaced by a nice loop.
     // output[1]
     // output[2]
     // output[3]
-
-
-
-  });
-});
